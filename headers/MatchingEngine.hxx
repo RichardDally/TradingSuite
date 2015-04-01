b53bc6ba2af369394ebc535547cbaae0e4cd9068
Richard Dally <leflou@gmx.com>
2015-04-01 12:10:04 +0200

    Implement MatchingEngine methods

--- headers/MatchingEngine.hxx
+++ headers/MatchingEngine.hxx
@@ -7,25 +7,67 @@
 template <typename InstrumentType, typename OrderTraits, typename InstrumentTraits>
 void MatchingEngine<InstrumentType, OrderTraits, InstrumentTraits>::CreateOrderBook(const InstrumentID& id)
 {
-	orderBooks_.insert({ id, std::move(OrderBookType()) });
+	if (orderBooks_.find(id) == orderBooks_.end())
+	{
+		orderBooks_.emplace(id, std::move(OrderBookType()));
+	}
 }
 
 template <typename InstrumentType, typename OrderTraits, typename InstrumentTraits>
 bool MatchingEngine<InstrumentType, OrderTraits, InstrumentTraits>::AddOrder(OrderType& order)
 {
-	return true;
+	bool result = false;
+
+	// Find order book related to instrument id
+	auto it = orderBooks_.find(order.instrumentID_);
+	if (it != orderBooks.end())
+	{
+		result = it->second->AddOrder(order);
+	}
+	else
+	{
+		// TODO: log an error about "instrument id has been not found"
+	}
+
+	return result;
 }
 
 template <typename InstrumentType, typename OrderTraits, typename InstrumentTraits>
-bool MatchingEngine<InstrumentType, OrderTraits, InstrumentTraits>::ModOrder(const OrderID& orderID, OrderType& newOrder)
+bool MatchingEngine<InstrumentType, OrderTraits, InstrumentTraits>::ModOrder(OrderType& newOrder)
 {
-	return true;
+	bool result = false;
+
+	// Find order book related to instrument id
+	auto it = orderBooks_.find(order.instrumentID_);
+	if (it != orderBooks.end())
+	{
+		result = it->second->ModOrder(newOrder);
+	}
+	else
+	{
+		// TODO: log an error about "instrument id has been not found"
+	}
+
+	return result;
 }
 
 template <typename InstrumentType, typename OrderTraits, typename InstrumentTraits>
-bool MatchingEngine<InstrumentType, OrderTraits, InstrumentTraits>::DelOrder(const OrderID& orderID)
+bool MatchingEngine<InstrumentType, OrderTraits, InstrumentTraits>::DelOrder(const OrderType& orderID)
 {
-	return true;
+	bool result = false;
+
+	// Find order book related to instrument id
+	auto it = orderBooks_.find(order.instrumentID_);
+	if (it != orderBooks.end())
+	{
+		result = it->second->DelOrder(orderID);
+	}
+	else
+	{
+		// TODO: log an error about "instrument id has been not found"
+	}
+
+	return result;
 }
 
 #endif
