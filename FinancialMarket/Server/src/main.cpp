4bc28eb36b4a613b837d9d57c74178fd2ecc16fc
Richard Dally <leflou@gmx.com>
2015-07-19 12:34:25 +0200

    Adding std::cin in a thread to stop the server

--- FinancialMarket/Server/src/main.cpp
+++ FinancialMarket/Server/src/main.cpp
@@ -1,11 +1,30 @@
+#include <atomic>
+#include <thread>
 #include <iostream>
 #include "OrderTraits.h"
 #include "InstrumentTraits.h"
 #include "StockInstrument.h"
 #include "FinancialMarket.h"
 
+void ReadCin(std::atomic<bool>& run)
+{
+    std::string buffer;
+
+    while (run.load())
+    {
+        std::cin >> buffer;
+        if (stricmp(buffer.c_str(), "Quit") == 0)
+        {
+            run.store(false);
+        }
+    }
+}
+
 int main()
 {
+    std::atomic<bool> run = true;
+    std::thread cinThread(ReadCin, std::ref(run));
+
     using SimpleInstrumentTraits = InstrumentTraits<int>;
     using DerivedInstrumentType = StockInstrument<SimpleInstrumentTraits>;
     using SimpleOrderIDType = int;
@@ -16,8 +35,13 @@ int main()
     FinancialMarket<DerivedInstrumentType, SimpleOrderTraits, SimpleInstrumentTraits> fmarket;
 	fmarket.LoadReferential();
 
-	int dummy;
-	std::cin >> dummy;
+    while (run.load())
+    {
+        // main loop
+    }
+    
+    run.store(false);
+    cinThread.join();
 
 	return 0;
 }
