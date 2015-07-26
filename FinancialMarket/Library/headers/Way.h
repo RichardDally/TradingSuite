7ce86056e75213ea8a20a044551c25386ef24a60
Richard Dally <leflou@gmx.com>
2015-07-26 15:58:53 +0200

    Enhance Way header by adding strings and more values

--- FinancialMarket/Library/headers/Way.h
+++ FinancialMarket/Library/headers/Way.h
@@ -4,8 +4,23 @@
 
 enum class Way : int
 {
-	BUY,
-	SELL
+    UNDEFINED,
+    BUY,
+    SELL,
+    MAX
 };
+
+static const char* const wayAsString[] =
+{
+    "UNDEFINED",
+    "BUY",
+    "SELL"
+};
+static_assert(sizeof(wayAsString) / sizeof(wayAsString[0]) == static_cast<size_t>(Way::MAX), "Way enum and strings are not matching");
+
+const char* const WayToString(const Way way)
+{
+    return wayAsString[static_cast<int>(way)];
+}
 
 #endif
